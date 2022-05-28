94f11235285077756bfcd8100f70bceab8b833c3
RequintelRoseAnn <roseannrequintel7@gmail.com>
2022-05-28 22:41:21 +0800

    Riddle list page Added

---
+++ riddle.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Riddles</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<header class="mb-auto p-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">WebRiddles</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="riddle.php">Riddles</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">About</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            
+</header>
+
+<body class="background">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        
+
+        <div class="container"><br><br>
+
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 1
+                        <a class="btn btn-primary btn-sm float-end ; " href="riddle1.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 2
+
+                        <a class="btn btn-primary btn-sm float-end " href="riddle2.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 3
+                        <a class="btn btn-primary btn-sm float-end" href="riddle3.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <div class="container1 rounded">Riddle 4
+                        <a class=" btn btn-primary btn-sm float-end " href="riddle4.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 5
+                        <a class="btn btn-primary btn-sm float-end" href="riddle5.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 6
+                        <a class="btn btn-primary btn-sm float-end" href="riddle6.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 7
+                        <a class="btn btn-primary btn-sm float-end" href="riddle7.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 8
+                        <a class="btn btn-primary btn-sm float-end" href="riddle8.php" role="button">See
+                            Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 9
+                        <a class="btn btn-primary btn-sm float-end" href="riddle9.php" role="button">See Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 10
+                        <a class="btn btn-primary btn-sm float-end" href="riddle10.php" role="button">See Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 11
+                        <a class="btn btn-primary btn-sm float-end" href="riddle11.php" role="button">See Riddle</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Riddle 12
+                        <a class="btn btn-primary btn-sm float-end" href="riddle12.php" role="button">See Riddle</a>
+                    </div>
+                </div>
+               
+            </div><br><br>
+        </div>
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
