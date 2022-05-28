d50c1e9f4b7c76d61966807bcbc1d4f48df34b2c
RequintelRoseAnn <roseannrequintel7@gmail.com>
2022-05-28 22:35:21 +0800

    Home Page

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Web app using Php ">
+    <meta name="author" content="Angel Mae Garcia">
+    <meta name="author" content="Rose Ann Requintel">
+    <meta name="application-name" content="WebRiddles">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<header class="mb-auto p-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">WebRiddles</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="riddle.php">RIDDLES</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">ABOUT</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="contact.php">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            
+</header>
+
+<body class="background">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+        <main class="px-3">
+            <h1>Explore and Discover different Riddles</h1><br><br>
+            <p class="lead">All is a riddle, and</p>
+            <p class="lead">The key to a riddle...</p>
+            <p class="lead">Is another riddle.</p>
+        </main>
+
+        <footer class="footer">
+            <p>Copyright &copy; 2022.</p>
+            <p>Angel Mae Garcia</p>
+            <p>Rose Ann Requintel</p>
+        </footer>
+    </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
